1fbb96cf0c477d281aa322b8121fbb418698e1b8
dmitrinesterenko <dneseterenko@xogrp.com>
2016-09-08 22:21:46 -0400

    Adding Keras basic install
    Illustrates installation problems I am experiencing
    Includes python 2.7 and 3.5 dockers
    Includes basic start from Keras info
    https://keras.io/#getting-started-30-seconds-to-keras

---
+++ random.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from keras.models import Sequential
+import numpy as np
+
+model = Sequential()
+model.add(Dense(1, input_dim=784, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+data = np.random.random((1000, 784))
+labels = np.random.randint(2, size=(1000, 1))
+
+# train the model, iterating on the data in batches
+# of 32 samples
+model.fit(data, labels, nb_epoch=10, batch_size=32)
